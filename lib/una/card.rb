bfdea171e82eaec676774cba1bb8666d3c1f3504
yuuko <charlotte.e.valentine@gmail.com>
2021-03-11 00:04:53 -0600

    add color and value attributes to card

--- lib/una/card.rb
+++ lib/una/card.rb
@@ -1,3 +1,23 @@
 class Una::Card
-
+  attr_accessor :color, :value
+  # variable `params` is a hash, containing symbols that represent attributes of a Card instance
+  def initialize(params={color: nil, value: nil})
+    acceptable_colors = ["blue", "green", "red", "yellow"]
+    if params[:color] != nil
+      unless acceptable_colors.include?(params[:color])
+        raise ArgumentError.new("Not a valid color") 
+      end
+      @color = params[:color]
+    else
+      @color = acceptable_colors.sample
+    end
+    if params[:value] != nil
+      # will be changed to include +2 and +4 at some point
+      unless (0..9).include?(params[:value])
+        raise ArgumentError.new("Not a valid value")
+      end
+    else
+      @value = (0..9).to_a.sample
+    end
+  end
 end
--- test/test_card.rb
+++ test/test_card.rb
@@ -2,5 +2,15 @@ require 'minitest/autorun'
 require 'una'
 
 class CardTest < Minitest::Test
+  def setup
+    @card = Una::Card.new()
+  end
 
+  def test_check_for_valid_color
+    assert_includes(["blue", "green", "red", "yellow"], @card.color) 
+  end
+
+  def test_check_for_valid_value
+    assert_includes(0..9, @card.value)
+  end
 end
